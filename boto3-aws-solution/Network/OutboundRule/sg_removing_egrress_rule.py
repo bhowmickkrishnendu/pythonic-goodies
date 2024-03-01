14364dafeedf1a0430876f0b0b69a31d6ffbb1b5
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-03-01 08:39:30 +0530

    ⚡nomen cluster comments added

--- boto3-aws-solution/Network/OutboundRule/sg_removing_egrress_rule.py
+++ boto3-aws-solution/Network/OutboundRule/sg_removing_egrress_rule.py
@@ -1,12 +1,24 @@
+# Network:Security:OutboundRules:Removal
+# This module removes outbound rules from a security group
+# It takes the following parameters:
+#   group_id: Network:Security:GroupId - The ID of the security group to remove the outbound rules from
+#   rules: Network:Security:OutboundRules:List - A list of outbound rules to be removed, where each rule is a dictionary containing:
+#       protocol: Network:Protocol - The protocol of the outbound rule to remove (e.g., 'tcp', 'udp', 'icmp')
+#       port_range: Network:Ports - A tuple specifying the start and end ports of the outbound rule to remove
+#       cidr_block: Network:IPAddress:CIDR - The CIDR block of the outbound rule to remove
+
 import boto3
 
+# Network:Security:OutboundRules:Removal
 def remove_outbound_rules(group_id, rules):
+    # Network:Client
     ec2 = boto3.client('ec2')
 
     for rule in rules:
+        # Network:Security:OutboundRule:Revocation
         response = ec2.revoke_security_group_egress(
-            GroupId = group_id,
-            IpPermissions = [
+            GroupId=group_id,
+            IpPermissions=[
                 {
                     'IpProtocol': rule['protocol'],
                     'FromPort': rule['port_range'][0],
@@ -19,13 +31,11 @@ def remove_outbound_rules(group_id, rules):
                 },
             ],
         )
-        print(f"Outbount rule removed for {rule['cidr_block']}.")
-    
+        print(f"Outbound rule removed for {rule['cidr_block']}.")
+
     return response
-# end def
 
-# Network:Security:OutboundRules:Removal
-
+# Network:Security:OutboundRules:Removal:Example
 if __name__ == "__main__":
     group_id = 'sg-0eb0590edd65f2941'
     rules = [
